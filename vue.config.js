096dd14098488af4a7c53b90262aaa6e227f26da
keo <380213949@qq.com>
2019-07-20 10:35:58 +0800

    部署项目

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
  module.exports = {
+   // 基本路径
+   baseUrl: './',
+   // 生产环境是否生成 sourceMap 文件
+   productionSourceMap: false,
+   // 服务器端口号
+   devServer: {
+     port: 8000,
+   },
    publicPath: process.env.NODE_ENV === "production" ? "/vue-cnode" : "/"
  };
-
-
